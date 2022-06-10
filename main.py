070e794603bc2b12488efce9adb4e5525da4097d
Regina Reynolds <regina.c.reynolds@gmail.com>
2022-06-10 15:02:37 -0400

    Continue formatting

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ Boxes will range in value from 0-8
 Possible square solutions will depend on other squares in the same row, column, and box
 Solved cells remove their solution as a possibility from unsolved cells in the same row/column/box
 If a cell has only one possible solution, that must be the solution for that cell.
-If an unsolved value in a group (row, column, box) has only one possible group cell it can appear in, it must appear in that cell.
+If an unsolved value in a group (row, column, box) has only one possible group cell it can appear in, it must appear in
+that cell.
 If the puzzle is still not solved at that point, further processing is necessary.
 """
 
@@ -63,6 +64,7 @@ def scrape_puzzle(difficulty):
         loading = app.pages.carousel.current_slide
         Thread(target=partial(loading.parse_puzzle, cells, driver)).start()
 
+
 def remove_same(grouping, index, solution, solved_list, *dt):
     """Remove solved cell values from potential solutions of cells in same grouping"""
     for cell in grouping[str(index)]["squares"]:
@@ -79,11 +81,12 @@ def remove_same(grouping, index, solution, solved_list, *dt):
                     cell.background_color = "green"
                     cell.text = str(cell.solution)
 
-                    # Remove newly solved cell value from grouping's unsolved value list    
+                    # Remove newly solved cell value from grouping's unsolved value list
                     puzzle.rows[str(cell.row)]["unsolved"].pop(str(cell.solution), None)
                     puzzle.columns[str(cell.column)]["unsolved"].pop(str(cell.solution), None)
                     puzzle.boxes[str(cell.box)]["unsolved"].pop(str(cell.solution), None)
-                    solved_list.append(cell)    
+                    solved_list.append(cell)
+
 
 def find_unsolved(grouping, solved_list, dt, box_group=False):
     """Find unsolved group values"""
@@ -120,22 +123,22 @@ def find_unsolved(grouping, solved_list, dt, box_group=False):
                 remove_same(puzzle.boxes, frequency[0].box, frequency[0].solution, solved_list)
 
             else:
-                # If numbers A and B can only go in squares C and D of a grouping, no 
+                # If numbers A and B can only go in squares C and D of a grouping, no
                 # other squares in the grouping can have A or B as possible solutions.
-                # More generally, when the same possible numbers can appear in the same 
-                # potential cells, you know those numbers CAN'T appear anywhere else in the 
+                # More generally, when the same possible numbers can appear in the same
+                # potential cells, you know those numbers CAN'T appear anywhere else in the
                 # row/box/column.
                 # Example:
                 #             1 A 7
                 # 6 X 2 X X X B C 8
                 #             3 5 D
-                # In the above example, the box containing squares squares A-D is missing a 
+                # In the above example, the box containing squares squares A-D is missing a
                 # 4. Without any 4s in intersecting rows/columns, the 4 in this box seems to
                 # be a potential solution for all of the squares A-D. However, this box is also
                 # missing 2, 6, and 9. The row intersecting squares B and C already has a 2
-                # and a 6. This means that squares B and C CANNOT contain 2 or 6, so they MUST 
+                # and a 6. This means that squares B and C CANNOT contain 2 or 6, so they MUST
                 # contain 4 or 9. This means squares C and D CANNOT contain 4 or 9 and instead
-                # MUST contain 2 or 6.                
+                # MUST contain 2 or 6.
 
                 # Find unsolved values that share the same potential containing cells
                 indices = [int(num)]
@@ -157,8 +160,6 @@ def find_unsolved(grouping, solved_list, dt, box_group=False):
                         for index in to_remove:
                             sq.possible_solutions.remove(index)
 
-                # TODO: Immediately remove solution from possible solutions of groupmates
-                # TODO: Remove unsolved value from unsolved value list. Will this cause a conflict by skipping over a dictionary value?
             if box_group:
                 # When you know the row or column within a box where a number must appear,
                 # you know that number CAN'T appear on that row or column in neighboring boxes
@@ -197,8 +198,10 @@ def find_unsolved(grouping, solved_list, dt, box_group=False):
                             if cell.box != int(key) and int(num) in cell.possible_solutions:
                                 cell.possible_solutions.remove(int(num))
 
+
 class Puzzle():
     """Consists of 9 rows, 9 columns, and 9 boxes, each of which contains 9 Square objects"""
+
     def __init__(self):
         self.rows = {}
         self.columns = {}
@@ -208,9 +211,39 @@ class Puzzle():
         """Add squares to puzzle class"""
         loop = 0
         while loop < 9:
-            row = {"squares": [], "unsolved": {"1": [], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": [], "9": []}}
-            column = {"squares": [], "unsolved": {"1": [], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": [], "9": []}}
-            box = {"squares": [], "unsolved": {"1": [], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": [], "9": []}}
+            row = {
+                "squares": [],
+                "unsolved": {"1": [],
+                             "2": [],
+                             "3": [],
+                             "4": [],
+                             "5": [],
+                             "6": [],
+                             "7": [],
+                             "8": [],
+                             "9": []}}
+            column = {
+                "squares": [],
+                "unsolved": {"1": [],
+                             "2": [],
+                             "3": [],
+                             "4": [],
+                             "5": [],
+                             "6": [],
+                             "7": [],
+                             "8": [],
+                             "9": []}}
+            box = {
+                "squares": [],
+                "unsolved": {"1": [],
+                             "2": [],
+                             "3": [],
+                             "4": [],
+                             "5": [],
+                             "6": [],
+                             "7": [],
+                             "8": [],
+                             "9": []}}
             for square in puzzle_squares:
                 if square.row == loop:
                     row["squares"].append(square)
@@ -236,8 +269,10 @@ class Puzzle():
         puzz_board = app.pages.carousel.slides[2]
         Thread(target=puzz_board.create_board).start()
 
+
 class Square(Button):
-    """Contains the row, column, and box in which the object is located, the potential solutions to the box, and the final solution once solved"""
+    """Contains the object's row, column, and box locations, potential solutions, and the final solution once solved"""
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.ID = None
@@ -249,9 +284,11 @@ class Square(Button):
         self.text = "X"
         self.background_color = "red"
 
+
 def change_page(new_page, *dt):
     """Change slide displayed in carousel"""
     app.pages.carousel.load_slide(app.pages.carousel.slides[new_page])
+
 
 class DifficultyScreen(Widget):
     """Widget containing difficulty selection buttons"""
@@ -282,6 +319,7 @@ class DifficultyScreen(Widget):
         super().__init__(**kwargs)
         for child in self.options.children:
             child.bind(on_press=self.callback)
+
 
 def change_load_screen(val, new_max, dt):
     """Change loading screen text and reset progress bar value"""
@@ -415,6 +453,7 @@ class FrameScreen(Widget):
 
 class SudokuApp(App):
     """The Kivy application class"""
+
     def build(self):
         self.pages = FrameScreen()
 
@@ -429,6 +468,7 @@ class SudokuApp(App):
         self.pages.carousel.add_widget(puzz_screen)
 
         return self.pages
+
 
 if __name__ == '__main__':
     # Initialize globals
